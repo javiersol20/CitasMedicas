fa8c12d5ea16409779a43fc3c76e55c1b4d1cc85
Javier Solis <javiersol2000@outlook.com>
2022-02-09 22:41:13 -0600

    change Register controller DNI

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'dni' => '0000000000000',
             'role' => 'patient',
             'password' => Hash::make($data['password']),
         ]);
